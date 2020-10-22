81fe8940c4276f7483b9f2188b22580b17e43d02
EgorK2606 <kramere26@gmail.com>
2020-10-22 11:57:59 +0200

    Third commit - the app is done and you can see bitcoin price

---
+++ ByteCoin Calculator/Models/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  ByteCoin Calculator
+//
+//  Created by Gregor Kramer on 22.10.2020.
+//  Copyright © 2020 Gregor Kramer. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Codable {
+    let rate: Double
+}
---
+++ ByteCoin Calculator/Models/CoinManager.swift
@@ -0,0 +1,59 @@
+//
+//  CoinManager.swift
+//  ByteCoin Calculator
+//
+//  Created by Gregor Kramer on 22.10.2020.
+//  Copyright © 2020 Gregor Kramer. All rights reserved.
+//
+
+import Foundation
+
+protocol CoinManagerDelegate {
+    func DidUpdatePrice(price: String, currency: String)
+    func DidFailWithError(error: Error)
+}
+
+struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.DidFailWithError(error: error!)
+                }
+                if let safeData = data {
+                    if let bitcoinPrice = self.parseJSON(safeData){
+                        let stringPrice = String(format: "%.0f", bitcoinPrice)
+                        self.delegate?.DidUpdatePrice(price: stringPrice, currency: currency)
+                    }
+                    
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            return lastPrice
+        } catch {
+            delegate?.DidFailWithError(error: error)
+            return nil
+        }
+        
+    }
+    
+}
